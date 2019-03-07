add9d5bcbe76cc9ff56e5586c99914fa5edac1b5
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2019-03-07 13:59:56 +0100

    Fixed bug in scipy Rbf calculation

--- rbf.py
+++ rbf.py
@@ -25,9 +25,10 @@ def rbf_scipy(X, beta):
     N = X.shape[0]
     D = X.shape[1]    
     rbf = Rbf(X[:,0], X[:,1], X[:,2], X[:,3], X[:, 4], beta)
+    #Xtuple = tuple([X[:, i] for i in range(D)])
     Xtuple = tuple([X[:, i] for i in range(D)])
 
-    return rbf(Xtuple)
+    return rbf(*Xtuple)
 
 
 # Cython  implementation of a Radial Basis Function (RBF) approximation scheme
